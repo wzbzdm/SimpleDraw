ce6166c187ec465b790a5b89621675bf9ddce70f
wzbzdm <wzbzdm123@gmail.com>
2024-11-07 13:19:36 +0800

    immediate redraw

--- SimpleDraw.cpp
+++ SimpleDraw.cpp
@@ -7,10 +7,6 @@
 
 #define MAX_LOADSTRING 100
 
-// 定时器
-#define REDRAW 1
-#define REDRAW_INTERVAL 16 // 16毫秒，即约60 FPS
-
 // 全局变量:
 HINSTANCE hInst;                                // 当前实例
 HWND hWnd;									    // 主窗口句柄
@@ -547,17 +543,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 	{
-		// 重绘定时器
-		//SetTimer(hWnd, REDRAW, REDRAW_INTERVAL, NULL);
 		break;
 	}
 	case WM_TIMER:
 	{
-		//if (wParam == REDRAW) {
-		//	// 使用定时器清空背景
-		//	InvalidateRect(hCanvasWnd, NULL, FALSE);
-		//}
-		//break;
+		break;
 	}
 	case WM_COMMAND:
 	{
@@ -810,7 +800,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	case WM_DESTROY:
-		//KillTimer(hWnd, REDRAW);
 		PostQuitMessage(0);
 		break;
 	default:
